74977e75816b895268042e111ac4feaeb10e8e9b
LucasMac13 <64612310+LucasMac13@users.noreply.github.com>
2020-08-19 21:26:58 -0300

    formulario principal

---
+++ src/Principal/PrjMenu1.java
@@ -0,0 +1,15 @@
+
+
+package Principal;
+
+public class PrjMenu1 
+{
+    public static void main(String[] args) 
+    { 
+                //Qual o form a ser aberto
+               FormLogin f1 = new FormLogin();
+              //  f1.setModal(true);
+                f1.setLocationRelativeTo(null); //Centraliza o frmLogin
+                f1.setVisible(true);
+    }
+}
